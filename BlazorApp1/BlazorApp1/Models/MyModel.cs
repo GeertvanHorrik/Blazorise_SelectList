7251ae7db0bd0b37e16680592a9d68528d7ef10a
Geert van Horrik <geert@catenalogic.com>
2024-02-01 11:52:39 +0100

    Set up project

---
+++ BlazorApp1/BlazorApp1/Models/MyModel.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp1.Models
+{
+    public class MyModel
+    {
+        public MyModel()
+        {
+                
+        }
+
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string Name { get; set; }
+
+        public string DisplayName { get => Name; }
+    }
+}
---
+++ BlazorApp1/BlazorApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using BlazorApp1;
+using Blazorise;
+using Blazorise.AntDesign;
+using Blazorise.Icons.FontAwesome;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// Theming / styling
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddAntDesignProviders()
+    .AddFontAwesomeIcons();
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
